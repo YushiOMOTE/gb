93649c0e09edc510c45ff46953e62099e34ebf08
Yushi OMOTE <yushiomote@gmail.com>
2018-11-09 23:13:52 +0900

    Add the rest instructions

--- inst.py
+++ inst.py
@@ -1,5 +1,7 @@
 import yaml
 import os
+import numpy as np
+
 
 # check nth-bit carry carry on add
 def ca(x, y, n):
@@ -17,14 +19,46 @@ def cs(x, y, n):
 	m = b - 1
 	c = (b + (x & m) - (y & m)) & b
 	if c == 0:
-		return 1
-	return 0
+		return 0
+	return 1
 
 
 def z(v):
 	if v == 0:
 		return 1
 	return 0
+
+
+def add_sp(cpu, v):
+	sp = cpu.regs.sp
+	if v >= 0:
+		vv = v
+		cpu.regs.f.h |= ca(sp, vv, 12)
+		cpu.regs.f.c |= ca(sp, vv, 16)
+		return cpu.regs.sp + vv
+	else:
+		vv = abs(v)
+		cpu.regs.f.h |= cs(sp, vv, 12)
+		cpu.regs.f.c |= cs(sp, vv, 16)
+		return cpu.regs.sp - vv
+
+
+def _rl(n, b):
+	b = n & (1 << (b - 1))
+	n <<= 1
+	if b:
+		n |= 1
+	n &= ((1 << b) - 1)
+	return n
+
+
+def _rr(n, b):
+	n &= ((1 << b) - 1)
+	b = n & 1
+	n >>= 1
+	if b:
+		n |= (1 << (b - 1))
+	return n
 
 
 base_tmpl = '''
@@ -68,56 +102,299 @@ ld_tmpl = '''
 	{} = {}
 '''
 
+# LGHL
+ldhl_tmpl = '''
+	# {1}
+	{0} = add_sp(cpu, cpu.fetch())
+	cpu.regs.f.z = 0
+	cpu.regs.f.n = 0
+'''
+
 # ADD16
 add16_tmpl = '''
-	cpu.regs.f.h |= ca({0}, {1}, 12)
-	cpu.regs.f.c |= ca({0}, {1}, 16)
-	{0} += {1}
+	v = {1}
+	cpu.regs.f.h |= ca({0}, v, 12)
+	cpu.regs.f.c |= ca({0}, v, 16)
+	{0} += v
+	cpu.regs.f.n = 0
+'''
+
+# ADD SP,n
+addsp_tmpl = '''
+	v = np.int8({1})
+	cpu.regs.sp = add_sp(cpu, v)
+	cpu.regs.f.z = 0
 	cpu.regs.f.n = 0
 '''
 
 # ADD8
 add8_tmpl = '''
-	cpu.regs.f.h |= ca({0}, {1}, 4)
-	cpu.regs.f.c |= ca({0}, {1}, 8)
-	{0} += {1}
+	v = {1}
+	cpu.regs.f.h |= ca({0}, v, 4)
+	cpu.regs.f.c |= ca({0}, v, 8)
+	{0} += v
 	cpu.regs.f.n = 0
 	cpu.regs.f.z = z({0})
 '''
 
 # ADC8
 adc8_tmpl = '''
+	v = {1}
 	c = cpu.regs.c
-	cpu.regs.f.h |= ca({0}, {1} + c, 4)
-	cpu.regs.f.c |= ca({0}, {1} + c, 8)
-	{0} += ({1} + c)
+	cpu.regs.f.h |= ca({0}, v + c, 4)
+	cpu.regs.f.c |= ca({0}, v + c, 8)
+	{0} += (v + c)
 	cpu.regs.f.n = 0
 	cpu.regs.f.z = z({0})
 '''
 
 # SUB8
 sub8_tmpl = '''
-	cpu.regs.f.h |= cs({0}, {1}, 4)
-	cpu.regs.f.c |= cs({0}, {1}, 8)
-	{0} -= {1}
+	v = {1}
+	cpu.regs.f.h |= cs({0}, v, 4)
+	cpu.regs.f.c |= cs({0}, v, 8)
+	{0} -= v
 	cpu.regs.f.n = 1
 	cpu.regs.f.z = z({0})
 '''
 
+# SBC8
+sbc8_tmpl = '''
+	v = {1}
+	cpu.regs.f.h |= cs({0}, v + c, 4)
+	cpu.regs.f.c |= cs({0}, v + c, 8)
+	{0} -= (v + c)
+	cpu.regs.f.n = 1
+	cpu.regs.f.z = z({0})
+'''
+
+# AND
+and_tmpl = '''
+	cpu.regs.a &= {}
+	cpu.regs.f.z = z(cpu.regs.a)
+	cpu.regs.f.n = 0
+	cpu.regs.f.h = 1
+	cpu.regs.f.c = 0
+'''
+
+# OR
+or_tmpl = '''
+	cpu.regs.a |= {}
+	cpu.regs.f.z = z(cpu.regs.a)
+	cpu.regs.f.n = 0
+	cpu.regs.f.h = 0
+	cpu.regs.f.c = 0
+'''
+
+# XOR
+xor_tmpl = '''
+	cpu.regs.a ^= {}
+	cpu.regs.f.z = z(cpu.regs.a)
+	cpu.regs.f.n = 0
+	cpu.regs.f.h = 0
+	cpu.regs.f.c = 0
+'''
+
+# CP
+cp_tmpl = '''
+	v = {0}
+	a = cpu.regs.a
+	cpu.regs.f.z = z(a - v)
+	cpu.regs.f.n = 1
+	cpu.regs.f.h |= cs(a, v, 4)
+	cpu.regs.f.c |= cs(a, v, 8)
+'''
+
+
+def _push(cpu, v):
+	cpu.mc[cpu.regs.sp - 1] = (v >> 8) & 0xff
+	cpu.mc[cpu.regs.sp - 2] = v & 0xff
+	cpu.regs.sp -= 2
+
+
+def _pop(cpu):
+	v = cpu.mc[cpu.regs.sp] & 0xff
+	v |= (cpu.mc[cpu.regs.sp + 1] << 8) & 0xff00
+	cpu.regs.sp += 2
+	return v
+
+
 # PUSH
 push_tmpl = '''
-	cpu.mc[cpu.regs.sp - 1] = ({0} >> 8) & 0xff
-	cpu.mc[cpu.regs.sp - 2] = {0} & 0xff
-	cpu.regs.sp -= 2
+	_push(cpu, {0})
 '''
 
 # POP
 pop_tmpl = '''
-	v = cpu.mc[cpu.regs.sp] & 0xff
-	v |= (cpu.mc[cpu.regs.sp + 1] << 8) & 0xff00
-	{0} = v
-	cpu.regs.sp += 2
+	{0} = _pop(cpu)
 '''
+
+# SWAP
+swap_impl = '''
+	v = {0}
+	h = (v >> 8) & 0xf
+	l = v & 0xf
+	{0} = (l << 8) + h
+	cpu.regs.f.z = z({0})
+	cpu.regs.f.n = 0
+	cpu.regs.f.h = 0
+	cpu.regs.f.c = 0
+'''
+
+# CPL
+cpl_tmpl = '''
+	cpu.regs.a ^= 0xff
+	cpu.regs.f.n = 0
+	cpu.regs.f.h = 0
+'''
+
+# CCF
+ccf_tmpl = '''
+	cpu.regs.f.c ^= 1
+	cpu.regs.f.n = 0
+	cpu.regs.f.h = 0
+'''
+
+# SCF
+scf_tmpl = '''
+	cpu.regs.f.c = 1
+	cpu.regs.f.n = 0
+	cpu.regs.f.h = 0
+'''
+
+# HALT
+halt_tmpl = '''
+	cpu.halt = True
+'''
+
+# STOP
+stop_tmpl = '''
+	cpu.stop = True
+'''
+
+# DI
+di_tmpl = '''
+	cpu.di = True
+'''
+
+# EI
+ei_impl = '''
+	cpu.ei = True
+'''
+
+# RLC
+rlc_tmpl = '''
+	cpu.regs.f.c = {0} & 0x80
+	{0} = _rl({0}, 8)
+	cpu.regs.f.z = z({0})
+	cpu.regs.f.n = 0
+	cpu.regs.f.h = 0
+'''
+
+# RL
+rl_tmpl = '''
+	v = {0} | (cpu.regs.f.c << 8)
+	{0} = _rl(v, 9) & 0xff
+	cpu.regs.f.c = v & 0x10
+	cpu.regs.f.z = z({0})
+	cpu.regs.f.n = 0
+	cpu.regs.f.h = 0
+'''
+
+# RRC
+rrc_tmpl = '''
+	cpu.regs.f.c = {0} & 1
+	{0} = _rr({0}, 8)
+	cpu.regs.f.z = z({0})
+	cpu.regs.f.n = 0
+	cpu.regs.f.h = 0
+'''
+
+# RR
+rr_tmpl = '''
+	v = {0} << 1 | cpu.regs.f.c
+	v = _rr(v, 9)
+	{0} = (v >> 1) & 0xff
+	cpu.regs.f.c = v & 1
+	cpu.regs.f.z = z({0})
+	cpu.regs.f.n = 0
+	cpu.regs.f.h = 0
+'''
+
+# BIT
+bit_tmpl = '''
+	cpu.regs.f.z = z({1} | 1 << {0})
+	cpu.regs.f.n = 0
+	cpu.regs.f.h = 1
+'''
+
+# SET
+set_tmpl = '''
+	{1} |= (1 << {0})
+'''
+
+# RES
+res_tmpl = '''
+	{1} &= np.uint8(~(1 << {0}))
+'''
+
+# JP x
+jp_tmpl = '''
+	cpu.regs.pc = {}
+'''
+
+# JP x,y
+jpif_tmpl = '''
+	if {}:
+		cpu.regs.pc = {}
+'''
+
+# JR x
+jr_tmpl = '''
+	cpu.regs.pc += np.int8({})
+'''
+
+# JR x,y
+jrif_tmpl = '''
+	if {}:
+		cpu.regs.pc += np.int8({})
+'''
+
+# CALL x
+call_tmpl = '''
+	_push(cpu, cpu.regs.pc)
+	cpu.regs.pc = {}
+'''
+
+# CALL x,y
+callif_tmpl = '''
+	if {}:
+		_push(cpu, cpu.regs.pc)
+		cpu.regs.pc = {}
+'''
+
+# RST
+rst_tmpl = '''
+	cpu.regs.pc = {}
+'''
+
+# RET
+ret_impl = '''
+	cpu.regs.pc = _pop(cpu)
+'''
+
+# RET x
+retif_impl = '''
+	if {}:
+		cpu.regs.pc = _pop(cpu)
+'''
+
+# RETI
+reti_impl = '''
+	cpu.regs.pc = _pop(cpu)
+	cpu.intr = True
+'''
+
 
 def _inc(cpu, r):
 	a = getattr(cpu.regs, r)
@@ -129,6 +406,19 @@ def _dec(cpu, r):
 	a = getattr(cpu.regs, r)
 	setattr(cpu.regs, r, a - 1)
 	return a
+
+
+def _evalf(s):
+	s = s.lower()
+	if s == 'z':
+		return f'cpu.regs.f.z'
+	elif s == 'nz':
+		return f'~cpu.regs.f.z'
+	elif s == 'c':
+		return f'cpu.regs.f.c'
+	elif s == 'nc':
+		return f'~cpu.regs.f.c'
+
 
 def _eval(s, off=None):
 	s = s.lower()
@@ -191,20 +481,29 @@ def _load():
 		# print(f'Generate {code:02x}: {op} {args}')
 
 		if op == 'ld' or op == 'ldh':
-			if code == 0xe2 or \
-			   code == 0xf2 or \
-			   op == 'ldh':
-				off = 0xff00
+			if code == 0xf8:
+				_i(code, 'ldhl', *args, off=off)
 			else:
-				off = None
+				if code == 0xe2 or \
+        code == 0xf2 or \
+        op == 'ldh':
+					off = 0xff00
+				else:
+					off = None
 
-			_i(code, 'ld', *args, off=off)
+				_i(code, 'ld', *args, off=off)
 
 		elif op == 'inc' or op == 'dec':
-			_i(code, f'{op}{bits}', *args, debug=True)
+			_i(code, f'{op}{bits}', *args)
 
 		elif op == 'push' or op == 'pop':
-			_i(code, op, *args, debug=True)
+			_i(code, op, *args)
+
+		elif op == 'add':
+			if code == 0xe8:
+				_i(code, 'addsp', *args, debug=True)
+			else:
+				_i(code, f'{op}{bits}', *args, debug=True)
 
 
 def op(cpu, op):
